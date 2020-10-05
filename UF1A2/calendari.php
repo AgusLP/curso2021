aac55100938ed93640910e99c3f549ee73579baa
Marc <72212892+mbalague@users.noreply.github.com>
2020-10-05 17:11:21 +0200

    Update calendari.php

--- UF1A2/calendari.php
+++ UF1A2/calendari.php
@@ -1,3 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CALENDARIO</title>
+</head>
+<body>
+    
+    <?php
+    $mes=date("m");
+    $año=date("Y");
+    $dia_semana= date("w", mktime(0,0,0, $mes, 1 $any));
+    $running_day= ()
+    $dias_mes=date("t", mktime(0, 0, 0, $mes,1,$any));
+    $semana= array("DL", "DM", "DC", "DJ", "DV", "DS", "DG");
+    $meses= array("ENERO", "FEBRERO", "MARZO", "ABRIL", "MAIG", "JUNIO", "JULIO", "AGOSTO", "SEPTIEMBRE", "OCTUBRE", "NOVIEMBRE", "DICIEMBRE");
 
-echo"<table border=1">
+    
+
+    ?>
