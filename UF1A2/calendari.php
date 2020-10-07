9344965458e556e766fa7a9989da0d7af930e4c6
Marc Balague <mbalaguer@fp.insjoaquimmir.cat>
2020-10-07 20:20:36 +0200

    calendario

--- UF1A2/calendari.php
+++ UF1A2/calendari.php
@@ -1,10 +1,20 @@
+<table>
+<td>DL</td>
+<td>DM</td>
+<td>DC</td>
+<td>DJ</td>
+<td>DV</td>
+<td>DS</td>
+<td>DG</td>
+</table>
+
+
+
+
+
 
 <?php
-$mes=date("n");
-$año=date(Y);
-$diaActual=date("j");
-$diasemana=date("w",mktime(0,0,0, $mes, $año))+7;
-$ultimodiames=date("d",(mktime(0,0,0,$mes)-1));
+
 $contador=1;
 
 
