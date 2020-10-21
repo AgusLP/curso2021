488a74b72f01d8007c10134795eff69163d3ce6d
Marc Balague <mbalaguer@fp.insjoaquimmir.cat>
2020-10-21 18:33:58 +0200

    primera parte

--- UF1A3/sessio2.php
+++ UF1A3/sessio2.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
-
+if ($_SERVER["REQUEST_METHOD"]=="POST"){
+    session_destroy();
+}
     if (isset($_SESSION["usuario"]) or isset($_SESSION["contraseña"])){
         echo $_SESSION["usuario"];
         echo $_SESSION["contraseña"];
@@ -11,3 +13,16 @@ session_start();
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="sessio2.php" method="post">
+        <input type="submit" value="Log out">
+    </form>
+</body>
+</html>
