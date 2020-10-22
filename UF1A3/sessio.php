54ecfdcee57fd5f7b8fc7f88988f8847bbe3a771
Marc Balague <mbalaguer@fp.insjoaquimmir.cat>
2020-10-22 17:05:29 +0200

    sessiones

--- UF1A3/sessio.php
+++ UF1A3/sessio.php
@@ -6,11 +6,13 @@ include "libreria.php";
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $_SESSION["usuario"]= $_REQUEST["nombre"];
     $_SESSION["contraseña"]= $_REQUEST["pass"];
+    setcookie("usercookie", sha1(md5($_REQUEST["nombre"])), time() + 365 * 24 * 60 * 60 );
+    setcookie("passcookie", sha1(md5($_REQUEST["pass"])), time() + 365 * 24 * 60 * 60 );
     $email = correo($_SESSION["usuario"]);
     $Comprovacioncontra = contraseña($_SESSION["contraseña"]);
-    if ($email = TRUE and $Comprovacioncontra = TRUE){
-        if ($_REQUEST["nombre"]== "marc@gmail.com" and $_REQUEST["pass"] == "abcd") {
-            header("Location: https://dawjavi.insjoaquimmir.cat/mbalague/curso2021/UF1A3/sessio2.php");
+    if ($email = TRUE && $Comprovacioncontra = TRUE ){
+        if ($_COOKIE["usercookie"]== sha1(md5("marc@gmail.com")) && $_COOKIE["passcookie"] == sha1(md5("abcd"))) {
+            header("Location: ./sessio2.php");
         }else{
             echo "Te has equivocado";
         }
@@ -18,6 +20,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         echo "No esta en el formato que tiene que ser";
     }
 }
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -30,6 +34,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <form action="sessio.php" method="post">
         Nombre: <input type="text" name="nombre"></br>
         Contraseña: <input type="password" name="pass">
+        Aceptar cookies: <input type="checkbox" name="Aceptar">
         <input type="submit" value="enviar">
     </form>
 </body>
