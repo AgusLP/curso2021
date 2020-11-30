75b513f8b930554e586a9c87f143a31302629105
hola <hola>
2020-11-30 17:44:12 +0100

    ProvaUF1_MarcBalague

---
+++ UF1/prova_uf1/B/home.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+if (isset($_SESSION["user"]) and $_SESSION["pass"]){
+    echo("Bienvenido: ".$_SESSION["user"]);
+    if(isset($_POST["adeu"])){
+        session_destroy();
+    }
+}else {
+    header("Location: index.php");
+}
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="home.php" method="post">
+        <input type="submit" name="adeu" value="Salir">
+    </form>
+</body>
+</html>
